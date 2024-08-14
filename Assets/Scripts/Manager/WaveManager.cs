d9034e08f836365c3bf32ccaf4c3c304ae43cafb
Won <nhamtrungtien2013@gmail.com>
2024-08-14 19:01:10 +0700

    feat: add wave segments

--- Assets/Scripts/Manager/WaveManager.cs
+++ Assets/Scripts/Manager/WaveManager.cs
@@ -1,9 +1,14 @@
+using NaughtyAttributes;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
     [Header(" Settings ")]
+    [SerializeField] private int _waveDuration;
+
+    [Header(" Waves ")]
     [SerializeField] private Wave[] _waves;
 }
 
@@ -11,11 +16,13 @@ public class WaveManager : MonoBehaviour
 public struct Wave
 {
     public string name;
-    public WaveSegment[] segments;
+    public List<WaveSegment> segments;
 }
 
 [Serializable]
 public struct WaveSegment
 {
+    [MinMaxSlider(0, 100)] public Vector2 tStartEndPercent;
+    public int spawnFrequence;
     public GameObject enemyPrefabs;
 }
