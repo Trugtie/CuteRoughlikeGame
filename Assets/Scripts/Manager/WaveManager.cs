9e308ea4ae0d2101cf4e637a119436da4f4253d1
Won <nhamtrungtien2013@gmail.com>
2024-08-16 15:25:44 +0700

    feat: first wave

--- Assets/Scripts/Manager/WaveManager.cs
+++ Assets/Scripts/Manager/WaveManager.cs
@@ -1,15 +1,59 @@
 using NaughtyAttributes;
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
     [Header(" Settings ")]
     [SerializeField] private int _waveDuration;
+    private float _timer;
+    private List<float> _localCounters = new List<float>();
 
     [Header(" Waves ")]
     [SerializeField] private Wave[] _waves;
+
+    private void Start()
+    {
+        _localCounters.Add(1);
+    }
+
+    private void Update()
+    {
+        if (_timer < _waveDuration)
+            ManageCurrentWave();
+    }
+
+    private void ManageCurrentWave()
+    {
+        Wave wave = _waves[0];
+
+        for (int i = 0; i < wave.segments.Count; i++)
+        {
+            WaveSegment currentSegment = wave.segments[i];
+
+            float tStart = currentSegment.tStartEndPercent.x / 100 * _waveDuration;
+            float tEnd = currentSegment.tStartEndPercent.y / 100 * _waveDuration;
+
+            bool isOutSizeTRange = _timer < tStart || _timer > tEnd;
+
+            if (isOutSizeTRange)
+                continue;
+
+            float timeSinceSegmentStart = _timer - tStart;
+
+            float spawmDelay = 1 / currentSegment.spawnFrequence;
+
+            if (timeSinceSegmentStart / spawmDelay > _localCounters[i])
+            {
+                Instantiate(currentSegment.enemyPrefab, Vector3.zero, Quaternion.identity, transform);
+                _localCounters[i]++;
+            }
+        }
+
+        _timer += Time.deltaTime;
+    }
 }
 
 [Serializable]
@@ -24,5 +68,5 @@ public struct WaveSegment
 {
     [MinMaxSlider(0, 100)] public Vector2 tStartEndPercent;
     public int spawnFrequence;
-    public GameObject enemyPrefabs;
+    public GameObject enemyPrefab;
 }
