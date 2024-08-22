9c1429754348ce97b18e259f4e4158d01700b87f
Won <nhamtrungtien2013@gmail.com>
2024-08-22 20:35:46 +0700

    feat: add wave transition applied button callback

--- Assets/Scripts/Manager/WaveTransitionManager.cs
+++ Assets/Scripts/Manager/WaveTransitionManager.cs
@@ -36,7 +36,23 @@ public class WaveTransitionManager : MonoBehaviour, IGameStateListener
 
             _upgradeButtons[i].Button.onClick.RemoveAllListeners();
 
-            _upgradeButtons[i].Button.onClick.AddListener(() => Debug.Log(randomStatString));
+            Action action = GetPerformActionFromStat(stat);
+
+            _upgradeButtons[i].Button.onClick.AddListener(() => action?.Invoke());
+
+        }
+    }
+
+    private Action GetPerformActionFromStat(Stats stat)
+    {
+        switch (stat)
+        {
+            case Stats.Attack:
+                return () => Debug.Log("Applied Attack");
+            case Stats.AttackSpeed:
+                return () => Debug.Log("Applied Attack Speed");
+            default:
+                return () => Debug.Log("Invalid Action");
 
         }
     }
