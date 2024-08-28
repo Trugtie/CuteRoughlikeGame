a889b5acf0a39bee1ce9f18f5af64249911ffe7a
Won <nhamtrungtien2013@gmail.com>
2024-08-28 15:29:59 +0700

    feat: add lifesteal

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -16,14 +16,38 @@ public class PlayerHealth : MonoBehaviour, IPlayerStatsDependency
     private float _maxHealth;
     private float _health;
     private float _armor;
+    private float _lifeSteal;
+
+    private void Start()
+    {
+        Enemy.OnAnyHit += EnemyHitCallBack;
+    }
+
+    private void OnDestroy()
+    {
+        Enemy.OnAnyHit -= EnemyHitCallBack;
+    }
+
+    private void EnemyHitCallBack(Vector2 positionDamgeText, int damge, bool isCriticalHit)
+    {
+        if (_health >= _maxHealth)
+            return;
+
+        float lifeStealValue = damge * _lifeSteal / 100;
+        float healthToAdd = Mathf.Min(lifeStealValue, _maxHealth - _health);
+
+        _health += healthToAdd;
+
+        Debug.Log("healtoadd: " + healthToAdd);
+
+        UpdateVisual();
+    }
 
     public void TakeDamge(int damge)
     {
         float realDamge = damge * Mathf.Clamp(1 - (_armor / 100), 0, 10000);
 
         realDamge = Mathf.Min(realDamge, _health);
-
-        Debug.Log("Real damge: " + realDamge);
 
         _health -= realDamge;
 
@@ -47,7 +71,7 @@ public class PlayerHealth : MonoBehaviour, IPlayerStatsDependency
         float healthValue = _health / _maxHealth;
         _healthBar.value = healthValue;
 
-        _healthText.SetText($"{_health} / {_maxHealth}");
+        _healthText.SetText($"{(int)_health} / {_maxHealth}");
     }
 
     public void UpdatePlayerStats(PlayerStatsManager playerStatsManager)
@@ -58,6 +82,7 @@ public class PlayerHealth : MonoBehaviour, IPlayerStatsDependency
 
         _health = _maxHealth;
         _armor = playerStatsManager.GetStatValue(Stats.Armor);
+        _lifeSteal = playerStatsManager.GetStatValue(Stats.Lifesteal);
 
         UpdateVisual();
     }
