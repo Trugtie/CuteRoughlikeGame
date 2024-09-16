f1dd2b0429e0f0bad9581a705985a4b195fb3a3a
Won <nhamtrungtien2013@gmail.com>
2024-09-16 19:03:43 +0700

    feat: add stats container slide

--- Assets/Scripts/UI/ShopUI.cs
+++ Assets/Scripts/UI/ShopUI.cs
@@ -16,7 +16,12 @@ public class ShopUI : MonoBehaviour
     [Header(" Stat Container ")]
     [SerializeField] private Button _statsButton;
     [SerializeField] private EventTrigger _overlayTrigger;
+    [SerializeField] private RectTransform _overlayRectTransform;
     [SerializeField] private RectTransform _statsContainerRect;
+
+    private float _overlayAlpha;
+    private Vector2 _statsContainerShowPos;
+    private Vector2 _statsContainerHidePos;
 
     private void OnEnable()
     {
@@ -35,8 +40,7 @@ public class ShopUI : MonoBehaviour
         entry.eventID = EventTriggerType.PointerDown;
         entry.callback.AddListener((data) => { OnOverlayButtonClick((PointerEventData)data); });
         _overlayTrigger.triggers.Add(entry);
-
-        ToggleStatsContainer(false);
+        _overlayAlpha = _overlayRectTransform.GetComponent<Image>().color.a;
 
         ShopManager.Instance.OnUpdateReroll += UpdateRerollVisual;
         CurrencyManager.Instance.OnUpdatedCurrency += UpdateRerollVisual;
@@ -58,6 +62,14 @@ public class ShopUI : MonoBehaviour
     {
         float width = Screen.width / (4 * _statsContainerRect.lossyScale.x);
         _statsContainerRect.offsetMax = new Vector2(width, _statsContainerRect.offsetMax.y);
+
+        _statsContainerShowPos = _statsContainerRect.anchoredPosition;
+        _statsContainerHidePos = _statsContainerShowPos + Vector2.left * width;
+
+        _statsContainerRect.anchoredPosition = _statsContainerHidePos;
+
+        _statsContainerRect.gameObject.SetActive(false);
+        _overlayRectTransform.gameObject.SetActive(false);
     }
 
     public void OnOverlayButtonClick(PointerEventData data)
@@ -75,8 +87,33 @@ public class ShopUI : MonoBehaviour
 
     private void ToggleStatsContainer(bool isShow)
     {
-        _statsContainerRect.gameObject.SetActive(isShow);
-        _overlayTrigger.gameObject.SetActive(isShow);
-    }
+        if (isShow)
+        {
+            _statsContainerRect.gameObject.SetActive(isShow);
+            _overlayRectTransform.gameObject.SetActive(isShow);
 
+            _overlayRectTransform.GetComponent<Image>().raycastTarget = true;
+
+            LeanTween.cancel(_statsContainerRect);
+            LeanTween.move(_statsContainerRect, _statsContainerShowPos, .3f)
+                .setEase(LeanTweenType.easeInCubic);
+
+            LeanTween.cancel(_overlayRectTransform);
+            LeanTween.alpha(_overlayRectTransform, _overlayAlpha, 0.3f).setRecursive(false);
+        }
+        else
+        {
+            _overlayRectTransform.GetComponent<Image>().raycastTarget = false;
+
+            LeanTween.cancel(_statsContainerRect);
+            LeanTween.move(_statsContainerRect, _statsContainerHidePos, .3f)
+                .setEase(LeanTweenType.easeOutCubic)
+                .setOnComplete(() => _statsContainerRect.gameObject.SetActive(isShow));
+
+            LeanTween.cancel(_overlayRectTransform);
+            LeanTween.alpha(_overlayRectTransform, 0f, 0.3f)
+                .setRecursive(false)
+                .setOnComplete(() => _overlayRectTransform.gameObject.SetActive(isShow));
+        }
+    }
 }
