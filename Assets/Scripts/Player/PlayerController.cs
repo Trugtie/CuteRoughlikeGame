d5429ee0fcf1effded3d05e6bd6edda6ca8ed6c0
Won <nhamtrungtien2013@gmail.com>
2024-10-25 17:09:32 +0700

    feat: add character animation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour, IPlayerStatsDependency, IGameStateListener
 {
+    public static PlayerController Instance { get; private set; }
+
     [Header("Elements")]
     [SerializeField] private WonMobileJoystick _joystick;
 
     [Header("Settings")]
     [SerializeField] private float _minSpeed;
     [SerializeField] private float _baseMoveSpeed;
-    private float _moveSpeed;
+    public float MoveSpeed { get; private set; }
+    public float PlayerRbVelocityMagnitude { get; private set; }
 
     private Rigidbody2D _playerRb;
 
     private void Awake()
     {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+
         _playerRb = GetComponent<Rigidbody2D>();
     }
 
@@ -26,16 +34,17 @@ public class PlayerController : MonoBehaviour, IPlayerStatsDependency, IGameStat
 
     private void Move()
     {
-        _playerRb.velocity = _joystick.GetMoveVector() * _moveSpeed * Time.deltaTime;
+        _playerRb.velocity = _joystick.GetMoveVector() * MoveSpeed * Time.deltaTime;
+        PlayerRbVelocityMagnitude = _playerRb.velocity.magnitude;
     }
 
     public void UpdatePlayerStats(PlayerStatsManager playerStatsManager)
     {
         float moveSpeedStatValue = playerStatsManager.GetStatValue(Stats.MoveSpeed) / 100;
 
-        _moveSpeed = _baseMoveSpeed * (1 + moveSpeedStatValue);
+        MoveSpeed = _baseMoveSpeed * (1 + moveSpeedStatValue);
 
-        _moveSpeed = Mathf.Max(_moveSpeed, _minSpeed);
+        MoveSpeed = Mathf.Max(MoveSpeed, _minSpeed);
     }
 
     public void GameStateChangedCallback(GameStates gameState)
--- Assets/Scripts/Player/PlayerVisual.cs
+++ Assets/Scripts/Player/PlayerVisual.cs
@@ -3,14 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator), typeof(SpriteRenderer))]
 public class PlayerVisual : MonoBehaviour
 {
+    private const string SPEED = "speed";
+
     [Header(" Elements ")]
     private SpriteRenderer _playerRenderer;
+    private Animator _animator;
 
     private void Awake()
     {
         _playerRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        _animator.SetFloat(SPEED, PlayerController.Instance.PlayerRbVelocityMagnitude);
+        _animator.speed = PlayerController.Instance.MoveSpeed;
     }
 
     private void Start()
